a4bac46eb9a7eb87e135bb95e206087722ccfc3d
Ivan <ivan.ushakov@gmail.com>
2020-02-21 21:25:08 +0300

    Initial Commit

---
+++ BigImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BigImageViewer
+//
+//  Created by  Ivan Ushakov on 21.02.2020.
+//  Copyright © 2020 Lunar Key. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
